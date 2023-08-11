badc2e07815eb104470482959e210be23b77db83
MatteoD <duttomatteo03@gmail.com>
2023-08-11 20:18:15 -0300

    modificaciones

--- index.js
+++ index.js
@@ -6,105 +6,114 @@ function convertidor(gradosFahrenheit) {
     return gradosCelsius.toFixed(2);
 }
 
-alert("¡Bienvenido al convertidor de temperaturas!");
+//mostrar botones
+function mostrarElemento(id) {
+    document.getElementById(id).style.display = "block";
+}
 
-let continuar = true; //controlar si bucles continúan o se detiene el programa
+//ocultar botones
+function ocultarElemento(id) {
+    document.getElementById(id).style.display = "none";
+}
+
+const saludo = document.getElementById("saludo");
+const iniciarBtn = document.getElementById("iniciarBtn");
+const contenedor = document.getElementById("contenedor");
+const maxBtn = document.getElementById("maxBtn");
+const minBtn = document.getElementById("minBtn");
+const historialLista = document.getElementById("historialLista");
+
 let temperaturasConvertidas = []; //guarda las temp convertidas a °C
-let temperaturasIngresadas = false; //saber si se ingresaron temp para convertir
 
-//bucle principal
-while (continuar) {
-    let nameU;
-    //bucle que pide nombre de usuario
-    do {
-    nameU = prompt("¿a quién tengo el gusto de ayudar?");
+//listener btn iniciar
+iniciarBtn.addEventListener("click", function() {
+    saludo.style.display = "none";
+    iniciarBtn.style.display = "none";
+    contenedor.style.display = "block";
+    instrucciones.textContent = "Ingresa el valor en grados Fahrenheit:";
+});
 
-    if (nameU === null) {
-        continuar = false;
-        break; 
-    } else if (nameU === "") {
-        alert("Necesito obligatoriamente un dato para continuar");
-    }
-    } while (nameU === "");
+//listener btn convertidor
+convertirBtn.addEventListener("click", function() {
+    const fahrenheit = parseFloat(fahrenheitInput.value);
+    if (!isNaN(fahrenheit)) {
+        const celsius = convertidor(fahrenheit);
+        temperaturasConvertidas.push(parseFloat(celsius));
 
-    if (!continuar) {
-        break;
-    }
+        // actualizar el historial en localStorage
+        const historial = JSON.parse(localStorage.getItem("historial")) || [];
+        historial.push({
+            fahrenheit: fahrenheit,
+            celsius: celsius
+        });
+        localStorage.setItem("historial", JSON.stringify(historial));
 
-    alert("Buen día " + nameU + ", ¿Sabías que en algunos países utilizan la escala Fahrenheit para medir la temperatura?");
-    alert("Así que si, por ejemplo, te encuentras en EEUU, esto puede ser confuso si estás acostumbrado a utilizar Celsius.");
-    alert("¡Pero tranquilo " +nameU + ", yo te ayudaré a cambiar tu temperatura de °F a °C!");
+        // muestra nueva temp en el historial
+        mostrarHistorialItem({ fahrenheit, celsius }, historial.length - 1);
 
-    //bucle que pide las temperaturas y realiza la conversión
-    do {
-    let fahrenheit = prompt("Ingresa el valor en grados Fahrenheit:");
-
-    if (fahrenheit === null) {
-        continuar = false;
-        break; 
-    } else if (fahrenheit === "") {
-        alert("No ingresaste ningún valor, inténtalo de nuevo");
-        continue; 
-    }
-
-    if (isNaN(parseFloat(Number(fahrenheit)))) {
-        alert("El valor ingresado no es válido. Por favor, ingresa únicamente números.");
-        continue; 
-    }
-
-    //convertir temp y agregarlas al array
-    let celsius = convertidor(fahrenheit);
-    temperaturasConvertidas.push(celsius);
-    temperaturasIngresadas = true;
-
-    //respuesta predeterminada según el valor de la conversión
-    let respuesta;
-
-    if (celsius >= 30) {
-        respuesta = "Te derretís del calor T-T";
-    } else if (celsius >= 22) {
-        respuesta = "Está para el fernet con hielo ;)";
-    } else if (celsius >= 15) {
-        respuesta = "Con una camperita se está bien";
-    } else if (celsius >= 3) {
-        respuesta = "Está frío che, salen los team invierno :)";
+        resultado.textContent = `${fahrenheit} grados Fahrenheit son equivalentes a ${celsius} grados Celsius.`;
+        mostrarElemento("resultado");
+        nuevaConversionBtn.style.display = "block";
+        if (temperaturasConvertidas.length >= 1) {
+            mostrarElemento("maxBtn");
+            mostrarElemento("minBtn");
+        }
+        fahrenheitInput.value ="";
     } else {
-        respuesta = "Se te congelan las ideas :(";
+        resultado.textContent = "Por favor, ingresa un valor válido.";
+        mostrarElemento("resultado");
     }
+});
 
-    alert(fahrenheit + " grados Fahrenheit son equivalentes a " + celsius + " grados Celsius " + respuesta);
+//btn convertir otra temp
+nuevaConversionBtn.addEventListener("click", function() {
+    fahrenheitInput.value = "";
+    ocultarElemento("resultado");
+    ocultarElemento("nuevaConversionBtn");
+    ocultarElemento("maxBtn");
+    ocultarElemento("minBtn");
+});
 
-    let opcion = confirm("¿Quieres convertir otro valor?");
-    if (!opcion) {
-        continuar = false;
-        break; 
-    }
-    } while (true);
+//decir temp max
+maxBtn.addEventListener("click", function() {
+    const temperaturaMaxima = Math.max(...temperaturasConvertidas);
+    resultado.textContent = `La temperatura más alta registrada fue: ${temperaturaMaxima} grados Celsius.`;
+    mostrarElemento("resultado");
+});
+
+//decir temp min
+minBtn.addEventListener("click", function() {
+    const temperaturaMinima = Math.min(...temperaturasConvertidas);
+    resultado.textContent = `La temperatura más baja registrada fue: ${temperaturaMinima} grados Celsius.`;
+    mostrarElemento("resultado");
+});
+
+// funcion para mostrar un elemento en el historial
+function mostrarHistorialItem(item, index) {
+    const historialItem = document.createElement("li");
+    historialItem.textContent = `${item.fahrenheit}°F = ${item.celsius}°C`;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Eliminar";
+    deleteButton.addEventListener("click", function() {
+        // borrar temp del historial
+        const historial = JSON.parse(localStorage.getItem("historial")) || [];
+        historial.splice(index, 1);
+        localStorage.setItem("historial", JSON.stringify(historial));
+
+        // borrar temp de la lista en la web
+        historialLista.removeChild(historialItem);
+    });
+
+    historialItem.appendChild(deleteButton);
+    historialLista.appendChild(historialItem);
 }
 
-//si se ingresaron temp, pregunta si desea saber el max o min
-if (temperaturasIngresadas) {
-    do {
-        let opcionMaxMin = prompt("¿Quieres saber el valor máximo o mínimo de las temperaturas convertidas? (Escribe 'max' o 'min'). Si presionas Cancelar, saldrás del programa.");
+// mostrar historial al cargar la pagina
+const historial = JSON.parse(localStorage.getItem("historial")) || [];
+historial.forEach((item, index) => {
+    mostrarHistorialItem(item, index);
+});
 
-        if (opcionMaxMin === "max") {
-            let temperaturaMaxima = Math.max(...temperaturasConvertidas);
-            alert("La temperatura más alta registrada fue: " + temperaturaMaxima + " grados Celsius");
-        } else if (opcionMaxMin === "min") {
-            let temperaturaMinima = Math.min(...temperaturasConvertidas);
-            alert("La temperatura más baja registrada fue: " + temperaturaMinima + " grados Celsius");
-        } else if (opcionMaxMin !== null) {
-            alert("Opción no válida. Por favor, escribe 'max' o 'min' para obtener el valor máximo o mínimo de las temperaturas.");
-        }
-
-        // Preguntar al usuario si quiere volver a ver el max o min
-        if (opcionMaxMin !== null) {
-            continuar = confirm("¿Quieres volver a averiguar el valor máximo o mínimo?");
-        } else {
-            continuar = false;
-        }
-    } while (continuar);
-}
-
-alert("¡Chau chau, espero volver a verte!");
-
+// borrar historial al cargar la pagina
+localStorage.clear();
